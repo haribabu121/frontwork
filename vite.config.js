d0a4be3a883ff6bf0117cb3d90370a8d3e8efa70
azsoftech <azsoftech2024@gmail.com>
2026-04-02 10:12:50 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://workends.vercel.app',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },

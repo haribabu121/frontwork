af194742f48828e3d021862ab0c009e8ea4dc89e
azsoftech <azsoftech2024@gmail.com>
2026-04-07 10:43:35 +0530

    fix: corrected vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets
+  base: '/', // Use relative paths for assets
   server: {
     proxy: {
       '/api': {
